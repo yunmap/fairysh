20f26b90d8185b253c013b07038eccf48f92e31f
yunmap <kalmpink@korea.ac.kr>
2018-06-05 22:13:44 +0900

    13

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from flask import Flask
 from flask import render_template
+from flask import request
 
 subscription_key = "b8b0351c901a48798f744ee8713b2595"
 assert subscription_key
@@ -13,8 +14,10 @@ def hello_world():
   vision_analyze_url = vision_base_url + "analyze"
 
 	# Set image_url to the URL of an image that you want to analyze.
-  image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
-	    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+  #image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
+	#    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+
+  image_url = request.form['submit_pic']
 
   headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
   params   = {'visualFeatures': 'Categories,Description,Color'}

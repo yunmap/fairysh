df2a9b5197f74de86700eb261cf3970b675b9094
yunmap <kalmpink@korea.ac.kr>
2018-06-06 01:15:14 +0900

    server don't understand proxy

--- main.py
+++ main.py
@@ -16,23 +16,6 @@ x=""
 
 @app.route('/')
 def hello_world():
-  vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
-  vision_analyze_url = vision_base_url + "analyze"
-
-	# Set image_url to the URL of an image that you want to analyze.
-  image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
-	    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
-
-  headers = {'Ocp-Apim-Subscription-Key': subscription_key }
-  params = {'visualFeatures': 'Categories,Description,Color'}
-  data = {'url': image_url}
-  response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
-  response.raise_for_status()
-
-  analysis = response.json()
-  
-  image_caption = analysis["description"]["captions"][0]["text"].capitalize()
-
   #return image_caption
   return render_template('index.html')
 

7bfb615c7f4f0c901e03f93711e08c8ab2554886
yunmap <kalmpink@korea.ac.kr>
2018-06-06 01:19:13 +0900

    60

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload_file():
   file = request.files['image']
-  f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+  
+  f = os.path.join(app.config['UPLOAD_FOLDER'], 'upload.jpg')
         # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
   file.save(f)
   
@@ -32,7 +33,7 @@ def upload_file():
 
   headers = {'Ocp-Apim-Subscription-Key': subscription_key }
   params = {'visualFeatures': 'Categories,Description,Color'}
-  data = {'url': 'uploads/'+file.filename}
+  data = {'url': 'uploads/upload.jpg'}
   response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
   response.raise_for_status()
 

b5fa9feed85ca6680e93149f2ce56ef4c2c86564
yunmap <kalmpink@korea.ac.kr>
2018-06-06 01:17:14 +0900

    51

--- main.py
+++ main.py
@@ -22,25 +22,24 @@ def hello_world():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    file = request.files['image']
-    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+  file = request.files['image']
+  f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
-    file.save(f)
-    
-    vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
-    vision_analyze_url = vision_base_url + "analyze"
-
-    headers = {'Ocp-Apim-Subscription-Key': subscription_key }
-    params = {'visualFeatures': 'Categories,Description,Color'}
-    data = {'url': 'uploads/'+file.filename}
-    response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
-    response.raise_for_status()
-
-    analysis = response.json()
+  file.save(f)
   
-    image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+  vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
+  vision_analyze_url = vision_base_url + "analyze"
 
-    return render_template('index.html', image_caption=image_caption)
+  headers = {'Ocp-Apim-Subscription-Key': subscription_key }
+  params = {'visualFeatures': 'Categories,Description,Color'}
+  data = {'url': 'uploads/'+file.filename}
+  response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
+  response.raise_for_status()
+
+  analysis = response.json()
+  image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+
+  return render_template('index.html', image_caption=image_caption)
     
 if __name__ == '__main__':
   app.run()

871dc7ea609001d01bce0d6f206e56e80cd37833
yunmap <kalmpink@korea.ac.kr>
2018-06-06 15:34:35 +0900

    abc

--- main.py
+++ main.py
@@ -15,23 +15,22 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-  image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
-    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
   vision_base_url = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/"
   vision_analyze_url = vision_base_url + "analyze"
 
+  image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/" + \
+    "Broadway_and_Times_Square_by_night.jpg/450px-Broadway_and_Times_Square_by_night.jpg"
+
   headers = {'Ocp-Apim-Subscription-Key': subscription_key }
   params = {'visualFeatures': 'Categories,Description,Color'}
-  data = {'url': f}
+  data = {'url': image_url}
   response = requests.post(vision_analyze_url, headers=headers, params=params, json=data)
   response.raise_for_status()
 
   analysis = response.json()
   image_caption = analysis["description"]["captions"][0]["text"].capitalize()
 
-
   return render_template('index.html', image_caption=image_caption)
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
